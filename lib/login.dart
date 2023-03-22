ab4856fc8530102c6e140a90b7fbe25f7429ba4a
Luiz <lui.h@live.com>
2023-03-21 21:09:10 -0300

    Login tela

--- lib/login.dart
+++ lib/login.dart
@@ -11,8 +11,52 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: ,
+      body: SafeArea(
+        child: Column(
+          children: [
+            // ignore: sized_box_for_whitespace
+            Container(
+              height: 200,
+              width: double.infinity,
+              child: Image.asset('img/logo.png'),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Senha',
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 15),
+                    ElevatedButton(
+                      onPressed: (){}, 
+                      child: const Text("Logar"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_daily/login.dart';
 
 void main(List<String> args) {
   runApp(MaterialApp(
